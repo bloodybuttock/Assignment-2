2ace26a0550c3b8d4ee3cdc53a99b65b33c09d89
bloodybuttock <jurukuncicpm@gmail.com>
2021-04-01 01:28:07 +0700

    update cors

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT =  process.env.PORT || 3000;
 
 const corsOptions = {
   origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  // methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   preflightContinue: false,
   credentials: true,
 }
